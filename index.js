bfde49aa6e4d321b9ad1e57cea553f2bcae631af
fatima-noor-shamsi <fatimanoorshamsi987@gmail.com>
2026-03-09 14:19:18 +0500

    port changed

--- index.js
+++ index.js
@@ -2,30 +2,24 @@ const express = require("express")
 const cors = require("cors")
 const mongoose = require("mongoose")
 const dotenv = require("dotenv")
-// const bodyParser = require("body-parser")
+
 const app = express()
 const Routes = require("./routes/route.js")
 
-const PORT = process.env.PORT || 5000
+dotenv.config()
 
-dotenv.config();
-
-// app.use(bodyParser.json({ limit: '10mb', extended: true }))
-// app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
-
-app.use(express.json({ limit: '10mb' }))
+app.use(express.json({ limit: "10mb" }))
 app.use(cors())
 
 mongoose
-    .connect(process.env.MONGO_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-    .then(console.log("Connected to MongoDB"))
-    .catch((err) => console.log("NOT CONNECTED TO NETWORK", err))
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connected to MongoDB")
+  })
+  .catch((err) => {
+    console.log("MongoDB connection error:", err)
+  })
 
-app.use('/', Routes);
+app.use("/", Routes)
 
-app.listen(PORT, () => {
-    console.log(`Server started at port no. ${PORT}`)
-})
+module.exports = app
